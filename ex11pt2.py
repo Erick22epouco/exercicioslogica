03161558c38198b3f024f867bdeea6fd11c29ab5
Erick22epouco <rodrigues40like@gmail.com>
2026-05-28 12:54:57 -0300

    Update ex11pt2.py

---
+++ ex11pt2.py
@@ -0,0 +1,11 @@
+alturaH=float(input("homem digite sua altura(ex:1.75):"))
+
+peso_idealH=(72.2 * alturaH)-58
+
+print("peso ideal para homem:",peso_idealH)
+
+alturaM=float(input("mulher digite sua altura(ex:1.75):"))
+
+peso_idealM=(72.2 * alturaM)-58
+
+print("peso ideal para mulher:",peso_idealM)
