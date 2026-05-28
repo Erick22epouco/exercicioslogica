638d5a7aa4a8a1317a4fd1a908604ee6901ebc5c
Erick22epouco <rodrigues40like@gmail.com>
2026-05-28 12:59:02 -0300

    Update ex9.py

---
+++ ex9.py
@@ -0,0 +1,11 @@
+primeiro=int(input("digite o primeiro numero inteiro:"))
+segundo=int(input("digite o segundo numero inteiro"))
+terceio=float(input("digite um numero real:"))
+
+produto=(primeiro*2)*(segundo/2)
+soma=(primeiro*3)+terceio
+cubo=terceio**3
+
+print("o produto da primeira com metade da segunda:",produto)
+print("soma do triplo da primeira com o terceiro:",soma)
+print("teceira elevado ao cubo:",cubo)
